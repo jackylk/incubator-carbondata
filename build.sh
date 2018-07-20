abc024458eecf8de826639d281bcb4e3a1e49471
wanganyang <wanganyang@bytedance.com>
2018-07-20 13:57:14 +0800

    update spark version

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn -DskipTests -Pbuild-with-format -Pspark-2.2 -Dspark.version=2.2.x-bd1 -Dhadoop.version=2.6.0-cdh5.4.4-bd3 clean package
+mvn -DskipTests -Pbuild-with-format -Pspark-2.2 -Dspark.version=2.2.x-bd2-SNAPSHOT -Dhadoop.version=2.6.0-cdh5.4.4-bd3 clean package
