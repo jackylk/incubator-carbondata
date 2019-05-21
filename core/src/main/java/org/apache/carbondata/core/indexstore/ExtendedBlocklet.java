5c294a89f4896cdad8b2d7c4340964a18b339edb
ravipesala <ravi.pesala@gmail.com>
2019-05-21 14:55:46 +0530

    Rebased

--- core/src/main/java/org/apache/carbondata/core/indexstore/ExtendedBlocklet.java
+++ core/src/main/java/org/apache/carbondata/core/indexstore/ExtendedBlocklet.java
@@ -44,17 +44,19 @@ public class ExtendedBlocklet extends Blocklet {
   }
 
   public ExtendedBlocklet(String filePath, String blockletId,
-      boolean compareBlockletIdForObjectMatching, ColumnarFormatVersion version) {
+      boolean compareBlockletIdForObjectMatching, ColumnarFormatVersion version, RangeColumnSplitMerger merger) {
     super(filePath, blockletId, compareBlockletIdForObjectMatching);
     try {
-      this.inputSplit = CarbonInputSplit.from(null, blockletId, filePath, 0, -1, version, null);
+      this.inputSplit =
+          CarbonInputSplit.from(null, blockletId, filePath, 0, -1, version, null, merger);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
-  public ExtendedBlocklet(String filePath, String blockletId, ColumnarFormatVersion version) {
-    this(filePath, blockletId, true, version);
+  public ExtendedBlocklet(String filePath, String blockletId, ColumnarFormatVersion version,
+      RangeColumnSplitMerger merger) {
+    this(filePath, blockletId, true, version, merger);
   }
 
   public BlockletDetailInfo getDetailInfo() {
@@ -109,11 +111,7 @@ public class ExtendedBlocklet extends Blocklet {
   }
 
   public RangeColumnSplitMerger getSplitMerger() {
-    return splitMerger;
-  }
-
-  public void setSplitMerger(RangeColumnSplitMerger splitMerger) {
-    this.splitMerger = splitMerger;
+    return inputSplit.getSplitMerger();
   }
 
   @Override public boolean equals(Object o) {
