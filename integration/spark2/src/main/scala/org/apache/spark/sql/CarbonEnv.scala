bdcff5e9311821f094d2d2d385289388b6087290
Venkata Ramana G <ramana.gollamudi@huawei.com>
2019-06-26 12:55:47 +0530

    Support reading image information using imageinfo udf.
    Image info structure
    - format STRING,
    - width  INT,
    - height INT
    
    Usage:
    
    CREATE TEMPORARY TABLE imagesourcetable USING binaryfile
       OPTIONS(path="xyz/imagesfolder");
    SELECT length, IMAGEINFO(content).height, IMAGEINFO(content).width,
       IMAGEINFO(content).format, content FROM imagesourcetable;

--- integration/spark2/src/main/scala/org/apache/spark/sql/CarbonEnv.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/CarbonEnv.scala
@@ -28,6 +28,7 @@ import org.apache.spark.sql.events.{MergeBloomIndexEventListener, MergeIndexEven
 import org.apache.spark.sql.execution.command.cache._
 import org.apache.spark.sql.execution.command.mv._
 import org.apache.spark.sql.execution.command.preaaggregate._
+import org.apache.spark.sql.execution.command.images.ImageInfoFunction
 import org.apache.spark.sql.execution.command.timeseries.TimeSeriesFunction
 import org.apache.spark.sql.hive._
 
@@ -100,6 +101,9 @@ class CarbonEnv {
 
     // added for handling timeseries function like hour, minute, day , month , year
     sparkSession.udf.register("timeseries", new TimeSeriesFunction)
+
+    //image info related udf
+    sparkSession.udf.register("imageinfo", new ImageInfoFunction)
 
     // register for cloud udf
     CloudUdfRegister.register(sparkSession)
