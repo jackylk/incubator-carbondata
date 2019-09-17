5f9869e3db5b89e0323385546c84a0544d0c70a4
Zhangshunyu <zhangshunyu@huawei.com>
2019-09-17 19:46:35 +0800

    remove fleet

--- fleet/hbase/fleet-hbase-language/src/test/java/com/huawei/cloudtable/leo/language/json/JSONElement.java
+++
@@ -1,9 +0,0 @@
-package com.huawei.cloudtable.leo.language.json;
-
-import com.huawei.cloudtable.leo.language.SyntaxTree;
-
-public abstract class JSONElement extends SyntaxTree.Node {
-
-  public abstract void accept(JSONElementVisitor visitor);
-
-}
