12277b1029f057b2b139bf649820e663430bd421
l00290379 <liuzhi5@huawei.com>
2019-11-21 17:22:34 +0800

    [BUG2019111700027] 解决“truncate后，表大小未更新”的问题。

--- integration/spark2/src/main/scala/org/apache/spark/sql/execution/command/mutation/CarbonTruncateCommand.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/execution/command/mutation/CarbonTruncateCommand.scala
@@ -17,13 +17,15 @@
 
 package org.apache.spark.sql.execution.command.mutation
 
-import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
-import org.apache.spark.sql.{CarbonEnv, Row, SparkSession}
-import org.apache.spark.sql.execution.command.{DataCommand, TruncateTableCommand}
-import org.apache.spark.sql.hive.CarbonRelation
+import java.util
 
 import org.apache.carbondata.common.exceptions.sql.MalformedCarbonCommandException
 import org.apache.carbondata.common.logging.LogServiceFactory
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
+import org.apache.spark.sql.execution.command.management.CarbonLoadDataCommand
+import org.apache.spark.sql.execution.command.{DataCommand, TruncateTableCommand}
+import org.apache.spark.sql.hive.CarbonRelation
+import org.apache.spark.sql.{CarbonEnv, Row, SparkSession}
 
 case class CarbonTruncateCommand(child: TruncateTableCommand) extends DataCommand {
   override def processData(sparkSession: SparkSession): Seq[Row] = {
@@ -44,7 +46,28 @@ case class CarbonTruncateCommand(child: TruncateTableCommand) extends DataComman
       throw new MalformedCarbonCommandException(
         "Unsupported truncate table with specified partition")
     }
-    sparkSession.sql(s"delete from ${dbName}.${tableName} ").collect()
+    // select an empty result set for get schema.
+    val tableSchema = sparkSession.sql(s"SELECT * FROM $dbName.$tableName WHERE false").schema
+    val tableColumnNamesBuilder = new StringBuilder
+    for (tableField <- tableSchema.fields) {
+      tableColumnNamesBuilder.append(tableField.name).append(',')
+    }
+    // overwrite table with a empty data set.
+    CarbonLoadDataCommand(
+      databaseNameOp = Option(dbName),
+      tableName = tableName,
+      factPathFromUser = null,
+      dimFilesPath = Seq.empty,
+      options = Map((
+        "fileheader",
+        tableColumnNamesBuilder.substring(0, tableColumnNamesBuilder.length() - 1)
+      )),
+      isOverwriteTable = true,
+      dataFrame = Option(sparkSession.createDataFrame(
+        new util.ArrayList[Row](),
+        tableSchema)
+      )
+    ).run(sparkSession)
     Seq.empty
   }
 
