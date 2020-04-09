2542bd0b9192ad9660f272749cb509dbe9594a46
Indhumathi27 <indhumathim27@gmail.com>
2020-04-09 15:31:24 +0530

    Rebase and fix comments

--- integration/spark/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataRDDFactory.scala
+++ integration/spark/src/main/scala/org/apache/carbondata/spark/rdd/CarbonDataRDDFactory.scala
@@ -687,7 +687,7 @@ object CarbonDataRDDFactory {
   def clearIndexFiles(carbonTable: CarbonTable, segmentId: String): Unit = {
     try {
       val segments = List(new Segment(segmentId)).asJava
-      IndexStoreManager.getInstance().getAllIndexes(carbonTable).asScala
+      IndexStoreManager.getInstance().getAllCGAndFGIndexes(carbonTable).asScala
         .filter(_.getIndexSchema.isIndex)
         .foreach(_.deleteIndexData(segments))
     } catch {
